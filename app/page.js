78c0d735a770944e7a2cd665bcb2f1af2c5d3caf
Khushi <117034852+khushigaba@users.noreply.github.com>
2024-06-17 22:35:20 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -121,6 +121,7 @@ export function Page() {
         <li><a href="/week-2">Week 2</a></li>
         <li><a href="/week-3">Week 3</a></li> 
         <li><a href="/week-4">Week 4</a></li>
+        <li><a href="/week-5">Week 5</a></li>
       </ul>
     </div>
   );
