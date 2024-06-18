cbe2d5102b59d03f4f374426d16e942f0048a8fb
Khushi <117034852+khushigaba@users.noreply.github.com>
2024-06-17 22:33:41 -0600

    Add files via upload

---
+++ app/week-5/item.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react';
+
+const Item = ({ name, quantity, category }) => {
+  return (
+    <li className="list-none bg-yellow-300 border border-yellow-400 rounded-lg p-4 m-2 w-72">
+      <div className="font-bold text-lg text-black">{name}</div>
+      <div className="text-black">Quantity: {quantity}</div>
+      <div className="text-black">Category: {category}</div>
+    </li>
+  );
+};
+
+export default Item;
+
+  
