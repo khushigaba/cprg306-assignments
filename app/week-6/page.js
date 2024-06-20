bb5d54ea1eba3882796f74f72dfe61874a893b67
Khushi <117034852+khushigaba@users.noreply.github.com>
2024-06-20 02:19:47 -0600

    Add files via upload

---
+++ app/week-6/page.js
@@ -0,0 +1,21 @@
+
+"use client";
+import React, { useState } from 'react';
+import NewItem from './new-item';
+import ItemList from './item-list';
+import itemsData from './item.json';
+
+export default function Page() {
+
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (item) => {
+    setItems([...items, item]);
+  };
+  return (
+    <div>
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items = {items} />
+    </div>
+  );
+}
